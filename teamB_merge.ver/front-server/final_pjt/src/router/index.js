c2763636736da37e48436ffb432cfa53281ace09
heejinssss <heejin.dev33@gmail.com>
2023-05-18 17:51:34 +0900

    0518

---
+++ teamB_merge.ver/front-server/final_pjt/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SignUpView from '@/views/SignUpView'
+import LogInView from '@/views/LogInView'
+import MovieView from '@/views/MovieView'
+import DetailView from '@/views/DetailView'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  
+  {
+    path: '/signup',
+    name: 'SignUpView',
+    component: SignUpView
+  },
+  
+  {
+    path: '/login',
+    name: 'LogInView',
+    component: LogInView
+  },
+  
+  {
+    path: '/movie',
+    name: 'MovieView',
+    component: MovieView
+  },
+
+  {
+    path: '/:id',
+    name: 'DetailView',
+    component: DetailView
+  },
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
