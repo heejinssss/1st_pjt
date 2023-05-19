81768284ba4c1e49ee6cb503e397d4b8e4f549c5
heejinssss <heejin.dev33@gmail.com>
2023-05-19 17:43:28 +0900

    크롤링

--- teamB/back-server/movies/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Movie, Tmdb
-
-class TmdbSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tmdb
-        fields = '__all__'
